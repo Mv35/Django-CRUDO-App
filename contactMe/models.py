12f11c8df101149fc9d7cb8ce5d53507f376fe4a
Milo <milo.volpicelli@gmail.com>
2022-07-31 03:12:03 +0200

    formatting

--- contactMe/models.py
+++ contactMe/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class ContactFormModel(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    category = models.CharField(max_length=100, choices=[('question', 'Question') , ('other', 'Other')])
+    category = models.CharField(max_length=100, choices=[
+                                ('question', 'Question'), ('other', 'Other')])
     subject = models.CharField(max_length=100, default='')
     body = models.TextField()
 
